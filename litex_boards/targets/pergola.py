154fd1797903dfb8fb934c34205aafd64bfdcf2f
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-09-30 00:58:24 +0200

    Pergola: Adjust so it works with the current build

--- litex_boards/targets/pergola.py
+++ litex_boards/targets/pergola.py
@@ -30,9 +30,8 @@ from litedram.phy import GENSDRPHY
 # CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(Module):
-    def __init__(self, platform, sys_clk_freq, with_usb_pll=False):
+    def __init__(self, platform, sys_clk_freq):
         self.clock_domains.cd_sys    = ClockDomain()
-        self.clock_domains.cd_sys_ps = ClockDomain(reset_less=True)
 
         # # #
 
@@ -43,18 +42,9 @@ class _CRG(Module):
         self.submodules.pll = pll = ECP5PLL()
         pll.register_clkin(clk16, 16e6)
         pll.create_clkout(self.cd_sys,    sys_clk_freq)
-        pll.create_clkout(self.cd_sys_ps, sys_clk_freq, phase=90)
-        self.specials += AsyncResetSynchronizer(self.cd_sys, ~pll.locked)
 
-        # USB PLL
-        if with_usb_pll:
-            self.submodules.usb_pll = usb_pll = ECP5PLL()
-            usb_pll.register_clkin(clk16, 16e6)
-            self.clock_domains.cd_usb_12 = ClockDomain()
-            self.clock_domains.cd_usb_48 = ClockDomain()
-            usb_pll.create_clkout(self.cd_usb_12, 12e6, margin=0)
-            usb_pll.create_clkout(self.cd_usb_48, 48e6, margin=0)
-
+        # TODO: Does not build with this enabled
+        # self.specials += AsyncResetSynchronizer(self.cd_sys, ~pll.locked)
 
 # BaseSoC ------------------------------------------------------------------------------------------
 
@@ -71,12 +61,11 @@ class BaseSoC(SoCCore):
             **kwargs)
 
         # CRG --------------------------------------------------------------------------------------
-        with_usb_pll = kwargs.get("uart_name", None) == "usb_acm"
-        self.submodules.crg = _CRG(platform, sys_clk_freq, with_usb_pll)
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
 
         # Leds -------------------------------------------------------------------------------------
         self.submodules.leds = LedChaser(
-            pads         = Cat(*[platform.request("user_led", i) for i in range(8)]),
+            pads         = platform.request_all("user_led"),
             sys_clk_freq = sys_clk_freq)
         self.add_csr("leds")
 
@@ -109,12 +98,12 @@ def main():
     parser = argparse.ArgumentParser(description="LiteX SoC on Pergola")
     parser.add_argument("--build", action="store_true", help="Build bitstream")
     parser.add_argument("--load",  action="store_true", help="Load bitstream")
-    parser.add_argument("--toolchain", default="trellis",   help="Gateware toolchain to use, trellis (default) or diamond")
-    parser.add_argument("--device",             dest="device",    default="LFE5U-12F", help="FPGA device, Pergola is populated with a LFE5U-12F by default")
-    parser.add_argument("--sys-clk-freq", default=50e6,           help="System clock frequency (default=50MHz)")
+    parser.add_argument("--toolchain", default="trellis", help="Gateware toolchain to use, trellis (default) or diamond")
+    parser.add_argument("--device", dest="device", default="LFE5U-12F", help="FPGA device, Pergola is populated with a LFE5U-12F by default")
+    parser.add_argument("--sys-clk-freq", default=50e6, help="System clock frequency (default=50MHz)")
     builder_args(parser)
-    trellis_args(parser)
     soc_core_args(parser)
+    trellis_args(parser)
     args = parser.parse_args()
 
     soc = BaseSoC(
@@ -122,15 +111,10 @@ def main():
         toolchain=args.toolchain,
         sys_clk_freq=int(float(args.sys_clk_freq)),
         **soc_core_argdict(args))
+
     builder = Builder(soc, **builder_argdict(args))
     builder_kargs = trellis_argdict(args) if args.toolchain == "trellis" else {}
     builder.build(**builder_kargs, run=args.build)
-
-    # Hack because Litex doesn't support the absence of "--spimode" flag to ecppack
-    import subprocess
-    build_path = os.path.join(builder.gateware_dir, soc.build_name)
-    subprocess.call(["ecppack", "{}.config".format(build_path), "--bit", "{}.bit".format(build_path)])
-    # End of hack
 
     if args.load:
         prog = soc.platform.create_programmer()
